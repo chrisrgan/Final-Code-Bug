0dd789b15e185f54026ba47271f30610c59234b1
chrisrgan <47266221+chrisrgan@users.noreply.github.com>
2019-05-18 15:35:50 -0700

    Update CodeBug.java

--- CodeBug.java
+++ CodeBug.java
@@ -1,6 +1,6 @@
 class CodeBug {
   public static void main(String[] args) {
-	int[] arr = new int[]{10,9,8,7,6,5,4,3,2,1};
+	int[] arr = new int[]{1,2,3,4,5,6,7,8,9,10};
     int N = arr.length; //N = 10
     int minLocation=arr.length-1;
     double min = arr[arr.length -1];
